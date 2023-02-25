9ca23c6317ae15a3865b0ed4658fa9ce20d7191e
juanoterot1 <118125751+juanoterot1@users.noreply.github.com>
2023-02-25 00:24:17 -0500

    Update Mergesort_1.py

--- Mergesort_1.py
+++ Mergesort_1.py
@@ -1,22 +1,20 @@
+import random
+import timeit
+
 def mergesort(lista):
-    # Comprobamos si la lista es vacía o solo tiene un elemento
     if len(lista) <= 1:
         return lista
-    
-    # Dividimos la lista en dos mitades
+
     mitad = len(lista) // 2
     izquierda = lista[:mitad]
     derecha = lista[mitad:]
-    
-    # Aplicamos recursivamente mergesort a cada mitad
+
     izquierda = mergesort(izquierda)
     derecha = mergesort(derecha)
-    
-    # Combinamos las dos mitades ordenadas en una lista ordenada
+
     return merge(izquierda, derecha)
-    
+
 def merge(izquierda, derecha):
-    # Combinamos las dos mitades ordenadas en una lista ordenada
     resultado = []
     i, j = 0, 0
     while i < len(izquierda) and j < len(derecha):
@@ -26,8 +24,23 @@ def merge(izquierda, derecha):
         else:
             resultado.append(derecha[j])
             j += 1
-            
+
     resultado += izquierda[i:]
     resultado += derecha[j:]
-    
+
     return resultado
+
+# Generamos una lista de 10 números aleatorios entre 0 y 99
+lista = [random.randint(0, 99) for i in range(10)]
+
+# Imprimimos la lista desordenada
+print("Lista desordenada:")
+print(lista)
+
+# Medimos el tiempo de ejecución de mergesort con timeit
+tiempo = timeit.timeit(lambda: mergesort(lista), number=1000)
+
+# Imprimimos la lista ordenada y el tiempo de ejecución en segundos
+print("Lista ordenada:")
+print(mergesort(lista))
+print(f"Tiempo de ejecución: {tiempo} segundos")
