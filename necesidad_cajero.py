1fe361a4323b2561eeb62dc2b7993d0591f83ca5
juanoterot1 <118125751+juanoterot1@users.noreply.github.com>
2023-05-05 11:15:16 -0500

    Update necesidad_cajero.py

--- necesidad_cajero.py
+++ necesidad_cajero.py
@@ -52,3 +52,15 @@ personas_a_atender = int(input("Ingrese la cantidad de personas que deben ser at
 tiempo_de_trabajo = int(input("Ingrese las horas de trabajo del cajero: "))
 
 print(necesidad_segundo_cajero(personas_a_atender, tiempo_de_trabajo))
+
+// Explicacion
+
+"""Este código es una simulación de la necesidad de un segundo cajero en una entidad bancaria. El programa solicita al usuario la cantidad de personas que deben ser atendidas y las horas de trabajo del cajero.
+
+El programa luego procede a simular la atención de los clientes en dos filas, fila1 y fila2, utilizando la función randint() del módulo random para generar aleatoriamente el tiempo que cada cliente requerirá para ser atendido. La lista fila1 contendrá los clientes que llegaron primero, mientras que la lista fila2 contendrá los clientes que llegaron después.
+
+Luego, el programa procede a simular la atención de los clientes por parte del cajero durante el tiempo de trabajo especificado por el usuario. En cada ciclo de bucle, el programa verifica si hay clientes en la fila1, si es así, atiende al cliente que ha estado esperando más tiempo y disminuye su tiempo de atención en 1 unidad. Si el tiempo de atención del cliente llega a cero, el cliente es eliminado de la fila. Si la fila1 está vacía, el programa atiende a un cliente de la fila2 siguiendo el mismo proceso.
+
+El programa cuenta la cantidad de clientes atendidos y luego verifica si la cantidad de clientes atendidos es mayor o igual a 250. Si es así, se considera que no es necesario un segundo cajero, de lo contrario, se informa que se necesita un segundo cajero.
+
+El programa devuelve una cadena de texto que indica la cantidad de clientes atendidos y si se necesita o no un segundo cajero."""
